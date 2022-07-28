f0285df61c5cd279abdec70b0c427d57b720c42d
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-28 11:43:11 -0500

    Agent test
    Adding jest test to agent

--- database/test/agent.test.js
+++ database/test/agent.test.js
@@ -1,13 +1,51 @@
-let config = {
-  logging: () => {},
-};
+// Globals
+let config = { logging: function () {} };
+let mockMetricModel = null;
+let mockAgentModel = null;
 let db = null;
 
+// Test setup
 beforeEach(async () => {
-  const setupDatabase = require('../');
+  mockMetricModel = {
+    belongsTo: jest.fn(),
+  };
+  mockAgentModel = {
+    hasMany: jest.fn(),
+  };
+
+  const setupDatabase = require('../index');
+
+  jest.mock('../model/agent', () => jest.fn(() => mockAgentModel));
+  jest.mock('../model/metric', () => jest.fn(() => mockMetricModel));
+
   db = await setupDatabase(config);
 });
 
-test('Test if agent exists', () => {
-  expect(db.Agent).toBeTruthy();
+// Clean up test
+afterEach(() => {
+  jest.resetModules();
+  jest.clearAllMocks();
+});
+
+// Agent test
+describe('Agent', () => {
+  test('The agent should exists', () => {
+    expect(db.Agent).toBeDefined();
+  });
+
+  test('setupAgentModel.hasMany should be called', () => {
+    expect(mockAgentModel.hasMany).toHaveBeenCalled();
+  });
+
+  test('setupMetricModel.belongsTo should be called', () => {
+    expect(mockMetricModel.belongsTo).toHaveBeenCalled();
+  });
+
+  test('setupAgentModel.hasMany should be called with mockMetricModel', () => {
+    expect(mockAgentModel.hasMany).toHaveBeenCalledWith(mockMetricModel);
+  });
+
+  test('setupMetricModel.belongsTo should be called with mockAgentModel', () => {
+    expect(mockMetricModel.belongsTo).toHaveBeenCalledWith(mockAgentModel);
+  });
 });
