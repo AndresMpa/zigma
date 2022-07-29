7530b2eb9755043c5640ee4eda9f41bf04638fe9
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-29 13:42:55 -0500

    Creation and updating
    Using a function to handle creation and updating on agent model

--- database/lib/angent.js
+++ database/lib/angent.js
@@ -2,8 +2,26 @@ module.exports = function setupAgent(AgentModel) {
   function findById(agentId) {
     AgentModel.findById(agentId);
   }
+  async function createOrUpdate(agent) {
+    const condition = {
+      where: {
+        uuid: agent.uuid,
+      },
+    };
+
+    const existingAgent = await AgentModel.findOne(condition);
+
+    if (existingAgent) {
+      const updated = await AgentModel.update(agent, condition);
+      return updated ? AgentModel.findOne(condition) : existingAgent;
+    }
+
+    const result = await AgentModel.create(agent);
+    return result.toJSON();
+  }
 
   return {
+    createOrUpdate,
     findById,
   };
 };
