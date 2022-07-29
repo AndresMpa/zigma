2382fdea284fda52ea00c00e1b7bf408abd8aebb
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-29 12:34:19 -0500

    Semantic
    Using a more semantic naming

--- database/test/agent.test.js
+++ database/test/agent.test.js
@@ -1,22 +1,17 @@
+// Requirements
+const { mockAgent, mockMetric } = require('./mocks');
+const { fixtureAgent } = require('./fixtures');
 // Globals
+let single = { ...fixtureAgent.single };
 let config = { logging: function () {} };
-let mockMetricModel = null;
-let mockAgentModel = null;
 let db = null;
 
 // Test setup
 beforeEach(async () => {
-  mockMetricModel = {
-    belongsTo: jest.fn(),
-  };
-  mockAgentModel = {
-    hasMany: jest.fn(),
-  };
-
   const setupDatabase = require('../index');
 
-  jest.mock('../model/agent', () => jest.fn(() => mockAgentModel));
-  jest.mock('../model/metric', () => jest.fn(() => mockMetricModel));
+  jest.mock('../model/metric', () => jest.fn(() => mockMetric));
+  jest.mock('../model/agent', () => jest.fn(() => mockAgent));
 
   db = await setupDatabase(config);
 });
@@ -27,25 +22,38 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-// Agent test
-describe('Agent', () => {
+// Test
+describe('Setup', () => {
   test('The agent should exists', () => {
     expect(db.Agent).toBeDefined();
   });
-
   test('setupAgentModel.hasMany should be called', () => {
-    expect(mockAgentModel.hasMany).toHaveBeenCalled();
+    expect(mockAgent.hasMany).toHaveBeenCalled();
   });
-
   test('setupMetricModel.belongsTo should be called', () => {
-    expect(mockMetricModel.belongsTo).toHaveBeenCalled();
+    expect(mockMetric.belongsTo).toHaveBeenCalled();
   });
-
-  test('setupAgentModel.hasMany should be called with mockMetricModel', () => {
-    expect(mockAgentModel.hasMany).toHaveBeenCalledWith(mockMetricModel);
+  test('setupAgentModel.hasMany should be called with mockMetric', () => {
+    expect(mockAgent.hasMany).toHaveBeenCalledWith(mockMetric);
   });
+  test('setupMetricModel.belongsTo should be called with mockAgent', () => {
+    expect(mockMetric.belongsTo).toHaveBeenCalledWith(mockAgent);
+  });
+});
 
-  test('setupMetricModel.belongsTo should be called with mockAgentModel', () => {
-    expect(mockMetricModel.belongsTo).toHaveBeenCalledWith(mockAgentModel);
+describe('Agent service', () => {
+  describe('FindById feature', () => {
+    test('Function should be called once', async () => {
+      await db.Agent.findById(single.id);
+      expect(mockAgent.findById).toBeCalledTimes(1);
+    });
+    test('Function should be called with the same id', async () => {
+      await db.Agent.findById(single.id);
+      expect(mockAgent.findById).toBeCalledWith(single.id);
+    });
+    test('Function should be exactly the same', async () => {
+      const agent = await mockAgent.findById(single.id);
+      expect(agent).toEqual(fixtureAgent.byId(single.id));
+    });
   });
 });
