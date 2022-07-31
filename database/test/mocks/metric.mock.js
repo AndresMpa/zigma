73fe22dfa10940125e7ae2c644b4131e19698cb1
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-30 19:02:21 -0500

    Metric mock
    Adding metric mock to make jest test

--- database/test/mocks/metric.mock.js
+++ database/test/mocks/metric.mock.js
@@ -1,3 +1,17 @@
+const { fixtureMetric } = require('../fixtures');
+
 module.exports = {
   belongsTo: jest.fn(),
+  findAll: jest.fn(async () => fixtureMetric.all),
+  findByAgentUUID: jest.fn(async (uuid) => fixtureMetric.findByAgentUUID(uuid)),
+  findByTypeAgentUUID: jest.fn(async (type, uuid) =>
+    fixtureMetric.findByTypeAgentUUID(type, uuid)
+  ),
+  create: jest.fn((single) =>
+    Promise.resolve({
+      toJSON() {
+        return single;
+      },
+    })
+  ),
 };
