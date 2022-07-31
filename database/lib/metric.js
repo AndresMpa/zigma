07eac1add8bce6b7057130a4927956a4c3af7523
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-30 19:00:40 -0500

    Doc about queries
    Adding queries to get some mental piece

--- database/lib/metric.js
+++ database/lib/metric.js
@@ -1,5 +1,6 @@
 function setupMetric(MetricModel, AgentModel) {
   async function create(uuid, metric) {
+    // SELECT uuid FROM AgentModel WHERE agent.uuid == uuid
     const agent = await AgentModel.findOne({
       where: { uuid },
     });
@@ -13,8 +14,11 @@ function setupMetric(MetricModel, AgentModel) {
 
   async function findByAgentUUID(uuid) {
     return MetricModel.findAll({
+      // SELECT type FROM MetricModel
       attributes: ['type'],
+      // GROUP BY type
       group: ['type'],
+      // JOIN AgentModel WHERE agent.uuid == uuid
       include: [
         {
           attributes: [],
@@ -30,12 +34,17 @@ function setupMetric(MetricModel, AgentModel) {
 
   async function findByTypeAgentUUID(type, uuid) {
     return MetricModel.findAll({
+      //SELECT id, type, value, createdAt FROM MetricModel
       attributes: ['id', 'type', 'value', 'createdAt'],
+      // WHERE type == type
       where: {
         type,
       },
+      // LIMIT 20
       limit: 20,
+      // ORDER BY createdAt DESC
       order: [['createdAt', 'DESC']],
+      // JOIN AgentModel WHERE agent.uuid == uuid
       include: [
         {
           attributes: [],
