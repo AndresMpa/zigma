49d474b474b9c1819bc9ca549ae6effe1cda2851
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-30 21:48:27 -0500

    Fix
    Fixing names on my config.db var

--- database/config/index.js
+++ database/config/index.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const config = {
   env: process.env.NODE_ENV || 'development',
   db: {
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    name: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    database: process.env.DB_NAME || 'zigma',
+    username: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASS || 'admin123',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: process.env.DB_DIALECT || 'postgres'
   },
+  port: process.env.DB_PORT || 3000,
 };
 
 module.exports = { config };
