08040718b2c09e68adcea5cf3f9601df055fe016
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-30 22:14:57 -0500

    CLI
    Using CLI options on Node to have a better deploy

--- database/setup.js
+++ database/setup.js
@@ -1,25 +1,34 @@
 const debug = require('debug')('zigma:databse:setup');
 const { handleFatalError } = require('./util');
+const cmdArgs = require('command-line-args');
 const { config } = require('./config');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const database = require('./');
 
+// CLI options
+const optionDefinitions = [{ name: 'yes', alias: 'y', type: Boolean }];
+const parsedArgv = cmdArgs(optionDefinitions);
+
+// Promp for humans
 const promp = inquirer.createPromptModule();
 
 async function setup() {
-  const answer = await promp([
-    {
-      type: 'confirm',
-      name: 'setup',
-      message: `${chalk.yellow(
-        '[DANGER]:'
-      )} This script will remove the entire data base, are you sure?`,
-    },
-  ]);
+  if (!parsedArgv.yes) {
+    const answer = await promp([
+      {
+        type: 'confirm',
+        name: 'setup',
+        message: `${chalk.yellow(
+          '[DANGER]:'
+        )} This script will remove the entire data base, are you sure?`,
+      },
+    ]);
 
-  if (!answer.setup) {
-    return console.log(`${chalk.blue('[CANCELLED]:')} Script did not continue`);
+    if (!answer.setup) {
+      console.log(`${chalk.blue('[CANCELLED]:')} Script did not continue`);
+      process.exit(0);
+    }
   }
 
   const dBConfig = {
